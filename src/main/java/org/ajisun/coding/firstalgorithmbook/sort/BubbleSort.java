03a49476b77da25b222fecb7156fdbaa1ce81617
AJiSun <sunj.guang@foxmail.com>
2021-05-28 16:39:52 +0800

    [fix]add bubble sort

---
+++ src/main/java/org/ajisun/coding/firstalgorithmbook/sort/BubbleSort.java
@@ -0,0 +1,22 @@
+package org.ajisun.coding.firstalgorithmbook.sort;
+
+/**
+ * @Copyright (c) 2021. Hand Enterprise Solution Company. All right reserved.
+ * @ProjectName: ajisun-leetcode
+ * @PackageName: org.ajisun.coding.firstalgorithmbook.sort
+ * @Date: 2021/5/28
+ * @author: ajisun
+ * @Email: jiguang.sun@hand-china.com
+ */
+
+/**
+ * 冒泡排序：
+ *
+ * 两个数字比较大小，然后会换位置
+ * 交换数字的次数和输入数据的排列顺序有关。假设出现某种极端情况，如输
+ * 入数据正好以从小到大的顺序排列，那么便不需要任何交换操作；反过来，输入数据要
+ * 是以从大到小的顺序排列，那么每次比较数字后便都要进行交换。因此，冒泡排序的时
+ * 间复杂度为 O(n2)。
+ */
+public class BubbleSort {
+}

ec1b3b3fa7ce5d31057e7feb1f17e4727b4198ba
AJiSun <sunj.guang@foxmail.com>
2021-05-29 22:48:22 +0800

    [fix]add insert sort

---
+++ src/main/java/org/ajisun/coding/firstalgorithmbook/sort/InsertSort.java
@@ -0,0 +1,20 @@
+package org.ajisun.coding.firstalgorithmbook.sort;
+
+/**
+ * @Copyright (c) 2021. ajisun. All right reserved.
+ * @ProjectName: ajisun-leetcode
+ * @PackageName: org.ajisun.coding.firstalgorithmbook.sort
+ * @Date: 2021/5/29
+ * @author: ajisun
+ * @Email: ajisun
+ */
+public class InsertSort {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}

7e0b57bcd25c582b050c0bfeab02d0c047288cca
AJiSun <sunj.guang@foxmail.com>
2021-05-29 22:53:37 +0800

    [fix]update

--- src/main/java/org/ajisun/coding/firstalgorithmbook/sort/InsertSort.java
+++ src/main/java/org/ajisun/coding/firstalgorithmbook/sort/InsertSort.java
@@ -1,6 +1,17 @@
 package org.ajisun.coding.firstalgorithmbook.sort;
 
 /**
+ * 在插入排序中，需要将取出的数据与其左边的数字进行比较。就跟前面讲的步骤一样，如果左边的数字更小，就不需要继续比较，本轮操作到此结束，
+ * 自然也不需要交换数字的位置。
+ *
+ * 然而，如果取出的数字比左边已归位的数字都要小，就必须不停地比较大小，交换数字，直到它到达整个序列的最左边为止。
+ *
+ *时间复杂度和冒泡排序的一样，都为 O(n²)
+ *
+ */
+
+
+/**
  * @Copyright (c) 2021. ajisun. All right reserved.
  * @ProjectName: ajisun-leetcode
  * @PackageName: org.ajisun.coding.firstalgorithmbook.sort

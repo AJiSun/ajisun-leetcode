be3f6ab70a5b5c6b8bcc369812b600816e4bc673
AJiSun <sunj.guang@foxmail.com>
2021-06-01 22:25:18 +0800

    [fix]add merge sort

--- src/main/java/org/ajisun/coding/firstalgorithmbook/sort/Heapsort.java
+++ src/main/java/org/ajisun/coding/firstalgorithmbook/sort/Heapsort.java
@@ -1,5 +1,6 @@
 package org.ajisun.coding.firstalgorithmbook.sort;
 
+
 /**
  * @Copyright (c) 2021. ajisun. All right reserved.
  * @ProjectName: ajisun-leetcode
@@ -9,4 +10,24 @@ package org.ajisun.coding.firstalgorithmbook.sort;
  * @Email: ajisun
  */
 public class Heapsort {
+
+    public static void main(String[] args) {
+
+        int m = 6;
+
+        System.out.println(Math.pow(6, 3));
+        System.out.println(m<<3);
+
+
+    }
+
+    static final int tableSizeFor(int cap) {
+        int n = cap - 1;
+        n |= n >>> 1;
+        n |= n >>> 2;
+        n |= n >>> 4;
+        n |= n >>> 8;
+        n |= n >>> 16;
+        return (n < 0) ? 1 : (n >= 1<<30) ? 1<<30 : n + 1;
+    }
 }
---
+++ src/main/java/org/ajisun/coding/firstalgorithmbook/sort/MergeSort.java
@@ -0,0 +1,15 @@
+package org.ajisun.coding.firstalgorithmbook.sort;
+
+/**
+ * @Copyright (c) 2021. ajisuny. All right reserved.
+ * @ProjectName: ajisun-leetcode
+ * @PackageName: org.ajisun.coding.firstalgorithmbook.sort
+ * @Date: 2021/6/1
+ * @author: ajisun
+ * @Email: ajisun
+ */
+public class MergeSort {
+    public static void main(String[] args) {
+
+    }
+}

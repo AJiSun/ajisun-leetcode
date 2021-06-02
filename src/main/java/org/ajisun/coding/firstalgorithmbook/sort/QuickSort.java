f8325a155d8d8a428605e4c5d063c047355d93bb
AJiSun <sunj.guang@foxmail.com>
2021-06-02 23:52:24 +0800

    [fix]update

---
+++ src/main/java/org/ajisun/coding/firstalgorithmbook/sort/QuickSort.java
@@ -0,0 +1,70 @@
+package org.ajisun.coding.firstalgorithmbook.sort;
+
+import java.util.Arrays;
+
+
+/**
+ * 快速排序是一种“分治法”。它将原本的问题分成两个子问题（比基准值小的数和
+ * 比基准值大的数），然后再分别解决这两个问题。子问题，也就是子序列完成排序后，
+ * 再像一开始说明的那样，把他们合并成一个序列，那么对原始序列的排序也就完成了。
+ * 不过，解决子问题的时候会再次使用快速排序，甚至在这个快速排序里仍然要使用
+ * 快速排序。只有在子问题里只剩一个数字的时候，排序才算完成。
+ */
+
+/**
+ * @Copyright (c) 2021. ajisun. All right reserved.
+ * @ProjectName: ajisun-leetcode
+ * @PackageName: org.ajisun.coding.firstalgorithmbook.sort
+ * @Date: 2021/6/2
+ * @author: ajisun
+ * @Email: ajisun
+ */
+public class QuickSort {
+
+
+    public static void main(String[] args) {
+        int[] nums = {5, 3, 8, 4, 2, 1, 9, 7, 6,500,100,300,200,400};
+
+        childrenQuickSort(nums,0,nums.length-1);
+        System.out.println(Arrays.toString(nums));
+
+    }
+
+
+    private static   void  childrenQuickSort(int[] nums,int left,int right ){
+
+        if (left<right){
+            int pivot = nums[left];
+            int i = left;
+            int j = right;
+            while (i<j){
+
+                while ((i<j) && nums[j]>=pivot){
+                    j--;
+                }
+                while ((i<j)&& nums[i]<=pivot){
+                    i++;
+                }
+
+                if (nums[i]==nums[j] && i<j){
+                    i++;
+                }else{
+                    nums[i] = nums[i] ^ nums[j];
+                    nums[j] = nums[i] ^ nums[j];
+                    nums[i] = nums[i] ^ nums[j];
+                }
+
+            }
+
+            nums[left] = nums[i];
+            nums[i] = pivot;
+            System.out.println(Arrays.toString(nums));
+            childrenQuickSort(nums,left,j-1);
+            childrenQuickSort(nums,i+1,right);
+
+        }
+
+    }
+
+
+}
